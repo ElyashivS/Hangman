9bf52e0c3dc47637389a537e7d820d3f77ac8d65
Elyashiv <Elyashiv2468@gmail.com>
2021-10-23 11:38:02 +0300

    Changed almost all methods

--- src/ChosenWord.java
+++ src/ChosenWord.java
@@ -1,17 +1,29 @@
 public class ChosenWord {
 
-    private String word;
+    private final String word;
+    private String blankWord;
 
     public String getWord() {
-        return word;
+        return blankWord;
     }
 
     public ChosenWord(String word) {
         this.word = word;
+        this.blankWord = "_".repeat(word.length());
     }
 
-    public String printUnderScores() {
-        String newWord = "_".repeat(word.length());
-        return newWord;
+    public String playerGuess(char guess) {
+
+        int index = word.indexOf(guess); // Index of guess. -1 if not in the word
+        if (index != -1) { // Check if the letter is in the word
+            char rightLetter = word.charAt(index);
+            blankWord = blankWord.substring(0, index) +
+                    rightLetter + blankWord.substring(index + 1);
+        }
+        return blankWord;
+    }
+
+    public boolean isGameOn() {
+        return blankWord.indexOf('_') != -1;
     }
 }
